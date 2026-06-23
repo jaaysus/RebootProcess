35adaac17b8e76188d28eee2afa7422c93e3d378
Issa El Kaddaoui <issaelkaddaoui@gmail.com>
2026-06-23 14:17:29 +0100

    migration db

---
+++ DynamicWiApi/Migrations/20260623131550_SyncPendingChanges.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DynamicWi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncPendingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HousingColor",
+                table: "EpnInfos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HousingColor",
+                table: "EpnInfos",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
